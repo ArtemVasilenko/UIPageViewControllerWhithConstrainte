b8cfaffc4b8ae020be5f53ba3947b0b256e8d126
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-04-15 19:25:29 +0300

    Initial Commit

---
+++ UIPageViewControllerWhithConstrainte/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPageViewControllerWhithConstrainte
+//
+//  Created by Артем on 4/15/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
